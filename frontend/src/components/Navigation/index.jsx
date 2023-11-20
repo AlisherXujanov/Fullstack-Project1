bf884d2a92bc007e32d37412cad95c38484bf17d
Alisher <alisherxujanov163@gmail.com>
2023-11-20 20:02:28 +0500

    upper-nav

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,5 +1,7 @@
 import { Outlet, Link } from 'react-router-dom'
 import styles from './style.scss'
+import emailVector from '../../assets/icons/email-vector.png'
+import phoneVector from '../../assets/icons/phone-vector.png'
 
 
 export default function Navigation() {
@@ -7,10 +9,20 @@ export default function Navigation() {
         <main style={styles}>
             <nav className='main-navbar'>
                 <div className="upper-nav">
-        
+                    <div className="left-nav">
+                        <a href="mailto:alisherxujanov163@gmail.com">
+                            <img src={emailVector} width="15" height="15" alt="" />
+                            alisherxujanov163@gmail.com
+                        </a>
+                        <span>
+                            <img src={phoneVector} width="15" height="15" alt="" />
+                            (+99833) 4747477
+                        </span>
+                    </div>
+                    <div className="right-nav"></div>
                 </div>
-                <Outlet />
             </nav>
+            <Outlet />
         </main>
     )
 }
