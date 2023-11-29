f49fc2980368e9ff4b66abb18f2f7fca2fc24182
Alisher <alisherxujanov163@gmail.com>
2023-11-29 18:56:24 +0500

    added toaster

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './index.scss'
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter } from 'react-router-dom'
-import { context, initialState, globalReducer } from './conf/store.js';
-import { useReducer } from 'react';
+import { context, initialState, globalReducer } from './conf/store.js'
+import { useReducer } from 'react'
 import AllComponents from './components/AllComponents.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const [state, dispatch] = useReducer(globalReducer, initialState)
@@ -11,6 +13,8 @@ function App() {
   return (
     <context.Provider value={state}>
       <div className="App">
+        <ToastContainer theme="dark" />
+
         <BrowserRouter>
           <AllComponents dispatch={dispatch} />
         </BrowserRouter>
--- frontend/src/components/AuthControl.jsx
+++ frontend/src/components/AuthControl.jsx
@@ -1,13 +1,16 @@
 import { isLoggedIn } from "../conf/common"
 import { useEffect } from "react"
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 export default function AuthControl(Component) {
+
     return function AuthenticatedComponent(props) {
         const navigate = useNavigate()
 
         useEffect(() => {
             if (!isLoggedIn()) {
+                toast.error('You are not logged in!', {toastId: '1'})
                 navigate('/auth')
             }
         }, [navigate])
