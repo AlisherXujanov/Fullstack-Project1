b74014452dc7ccdb233f3decdb0a411378eeef05
Alisher <alisherxujanov163@gmail.com>
2023-12-01 19:39:57 +0500

    Register form

--- backend/users/api_views.py
+++ backend/users/api_views.py
@@ -14,6 +14,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 @api_view(['POST'])
 def register(request):
     username = request.data.get('username')
+    email = request.data.get('email')
     password = request.data.get('password')
     password2 = request.data.get('password2')
     if password != password2:
@@ -22,7 +23,9 @@ def register(request):
         return Response({'error': 'Please provide both username and password'}, status=400)
     if User.objects.filter(username=username).exists():
         return Response({'error': 'Username already taken'}, status=400)
-    user = User.objects.create_user(username=username, password=password)
+    if not email:
+        return Response({'error': 'Please provide email'}, status=400)
+    user = User.objects.create_user(username=username, password=password, email=email)
     Token.objects.create(user=user)
     return Response({'success': True}, status=201)
 
--- frontend/src/components/Authentication/Register.jsx
+++ frontend/src/components/Authentication/Register.jsx
@@ -1,7 +1,52 @@
 import Eye from "../../assets/icons/Eye.png";
 import { useState } from "react";
+
+
 function Register(props) {
-    let [showPassword, setShowPassword] = useState(false);
+    let [showPassword, setShowPassword] = useState(false)
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState({})
+
+    const PATTERN = /^[a-zA-Z0-9]+$/;
+
+    function fireSetUsername(e) {
+        const val = e.target.value
+        if (val.length !== 0) {
+            if (!PATTERN.test(val)) {
+                setError({ ...error, usernameErr: 'Никнейм может содержать только латинские буквы и цифры' })
+            } else {
+                setError({ ...error, usernameErr: '' })
+            }
+        } else {
+            setError({ ...error, usernameErr: '' })
+        }
+        setUsername(val) 
+    } 
+    function fireSetPassword(e) {
+        const val = e.target.value
+        const inputName = e.target.name
+
+     
+        let errName = inputName === "password"  ?  'passwordErr' : 'password2Err'
+
+        if (val.length !== 0) {
+            if (!PATTERN.test(val)) {
+                setError({ ...error, [errName]: 'Пароль может содержать только латинские буквы и цифры' })
+            } else {
+                setError({ ...error, [errName]: '' })
+            }
+        } else {
+            setError({ ...error, [errName]: '' })
+        }
+
+        inputName === "password"  ?  setPassword(val) : setPassword2(val) 
+    } 
+   
+
     return (
         <div>
             <form className="auth-form">
@@ -10,11 +55,22 @@ function Register(props) {
                     <input type="text" id="register-email-input" placeholder="Электронная почта" pattern='^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$' />
                 </div>
                 <div>
-                    <input type={showPassword ? "text" : "password"} id="register-pass-input" className="pass-input" placeholder="Придумайте пароль" pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}' />
+                    <input 
+                        id="register-pass-input" 
+                        type={showPassword ? "text" : "password"} 
+                        className="pass-input" 
+                        placeholder="Придумайте пароль" 
+                        name="password"
+                    />
                     <img src={Eye} onClick={() => { setShowPassword(!showPassword) }} alt="" />
                 </div>
                 <div>
-                    <input type="password" id="pass2-input" placeholder="Повторите пароль" pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}' />
+                    <input 
+                        id="pass2-input" 
+                        type="password" 
+                        placeholder="Повторите пароль" 
+                        name="password2"
+                    />
                 </div>
                 <button className='войти'>Создать</button>
             </form>
